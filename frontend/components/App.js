361a077148a87d736aacd33155573205cf6d9ab8
William Johnson <catfishfishcat10101@gmail.com>
2023-08-17 19:05:48 -0500

    renders something now...

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,15 +6,33 @@ const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
-  return (
-    <div>
-      <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
-    </div>
-  )
+ // ❗ Create state to hold the data from the API
+ const [characters, setCharacters] = React.useState([])
+
+ // ❗ Create effects to fetch the data and put it in state
+ React.useEffect(() => {
+   const fetchCharacters = async () => {
+     try {
+       const response = await axios.get(urlPeople)
+       setCharacters(response.data)
+     } catch (error) {
+       console.error('Error fetching characters:', error)
+     }
+   }
+
+   fetchCharacters()
+ }, [])
+
+ return (
+   <div>
+     <h2>Star Wars Characters</h2>
+     <p>See the README of the project for instructions on completing this challenge</p>
+     {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+     {characters.map(character => (
+       <Character key={character.id} {...character} />
+     ))}
+   </div>
+ )
 }
 
 export default App
